b8cc966e1675ae8c8790f9adfb8a3d0aa372a90e
Adina <marativnaadina@gmail.com>
2023-04-22 01:17:24 +0600

    в процессе

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import React, {useEffect, useState} from "react";
 import './App.css';
+import MovieBox from "./MovieBox";
 
 function App() {
+  const [movies, setMovies]=useState([]);
+  const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=15c0395faab321ba84a13913286e91ef";
+
+  useEffect(()=>{
+    fetch(API_URL)
+        .then((res)=>res.json())
+        .then(data=>{
+          console.log(data);
+          setMovies(data.results);
+        })
+  }, [])
   return (
     <div >
-      Movie App
+        {movies.map((movieReq)=><MovieBox/>)}
     </div>
   );
 }
